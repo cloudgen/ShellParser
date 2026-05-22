33f897989cd97ec5d185b0b6d9a5b5006f65bd77
cloudgen <wongcf22@gmail.com>
2026-05-22 11:59:28 +0800

    1.2.0 Batch update

--- build/lib/setup.py
+++
@@ -1,7 +0,0 @@
-#!/home/adm01/.pyenv/shims/python3
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-  ext_modules=cythonize("src/ShellParser.pyx",compiler_directives={"language_level" : "3"})
-)
